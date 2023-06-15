173c314ff08631dc33d507d126a8ad7139f4c793
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-06-15 11:37:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const players = ["John", "Bob", "Alice", "Poppy"];
+const players = ['John', 'Bob', 'Alice', 'Poppy'];
 
 const person = {
-  name: "John Doe",
-  age: 80,
-};
+      name: 'John Doe',
+      age: 80
+    };
 
-const team = players;
-const team1 = players.slice();
-const cap1 = { ...person };
+// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+var team = players;
+var team1 = [...players];
+var cap1 = {...person};
 
-console.log(team);  // Output: ["John", "Bob", "Alice", "Poppy"]
-console.log(team1); // Output: ["John", "Bob", "Alice", "Poppy"]
-console.log(cap1);  // Output: { name: "John Doe", age: 80 }
+export {players,person,team,team1,cap1}
