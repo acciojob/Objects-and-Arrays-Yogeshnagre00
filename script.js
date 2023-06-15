edd00b2387156f5ddb3cc8af0b5d9054e2ea6226
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-06-15 12:31:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ const person = {
   name: "John Doe",
   age: 80,
 };
-const team=players;
-let team1=players;
 
-let cap1=person;
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-
+var team= players;
+var team1 =players.slice();
+var cap1 = Object.assign({}, person);
 window.players = players;
 window.person = person;
 window.team = team;
